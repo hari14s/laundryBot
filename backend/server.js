75d66f4d55e83bf8eef10c48688cade5b3a8d24a
hari14s <155997666+hari14s@users.noreply.github.com>
2025-05-26 13:29:15 +0530

    new commit

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,4 @@
 import express from "express";
-import session from 'express-session';
 import dotenv from "dotenv";
 import cors from "cors";
 import connectDB from "./utils/db.js";
@@ -21,16 +20,6 @@ app.use(cors({
   credentials: true
 }));
 
-app.use(session({
-  secret: process.env.SESSION_SECRET,
-  resave: false,
-  saveUninitialized: true,
-  cookie: {
-    secure: false, // set to true only if HTTPS (production)
-    httpOnly: true,
-    maxAge: 1000 * 60 * 15,
-  }
-}));
 
 app.use('/auth/dauth', dauthRoutes);
 
